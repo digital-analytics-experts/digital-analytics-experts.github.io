b8417c7f96608f19e9bf9efbed0846b33bbabc6d
digital-analytics-experts <34029702+digital-analytics-experts@users.noreply.github.com>
2017-12-06 13:04:27 +0000

    Add files via upload

---
+++ adobe-user-mgmt/js/config.js
@@ -0,0 +1,55 @@
+(function () {
+    window.MarketingCloud = window.MarketingCloud || {};
+    window.MarketingCloud.config = {};
+    
+    window.MarketingCloud.config.primary = {
+    	login: "#primary-login",
+		secret: "#primary-secret",
+		endpoint: "#primary-endpoint",
+		groupName: "#groupName",
+		groupDescription: "#groupDescription",
+		importBtn: "#primary-createUsers",
+		modifyBtn: "#primary-modifyUsers",
+		exportBtn: "#primary-downloadUsers",
+		deleteBtn: "#primary-deleteUsers",
+		compareCsvBtn: "#primary-downloadDiff",
+		getRsidsBtn: "#primary-getRsid",
+		createGroupBtn: "#primary-createGroup",
+		getGroupsBtn: "#primary-getGroups",
+		getRsidsBtn: "#primary-getRsid"
+		
+    }
+	
+	window.MarketingCloud.config.groups = {
+		rsidList : "#groupRsid",
+		categoryList : "#groupCategories",
+		description : "#groupDescription",
+		name: "#groupName",
+		groupList: "#groupList",
+		copyGroupBtn: "#copyGroup"
+	}
+
+    window.MarketingCloud.config.secondary = {
+    	login: "#secondary-login",
+		secret: "#secondary-secret",
+		endpoint: "#secondary-endpoint",
+		importBtn: "#secondary-createUsers",
+		modifyBtn: "#secondary-modifyUsers",
+		exportBtn: "#secondary-downloadUsers",
+		deleteBtn: "#secondary-deleteUsers",
+		getRsidsBtn: "#secondary-GetRsid",
+		createGroupBtn: "#secondary-createGroup",
+		getGroupsBtn: "#secondary-getGroups",
+		getRsidsBtn: "#secondary-getRsid",
+		copyGroupBtn: "#secondary-copyGroup"
+    }
+	
+	window.MarketingCloud.config.fileInput = {
+    	importUsersInput: "#importUsersInput",
+		importModifyUsersInput: "#importModifyUsersInput",
+		deleteUsersInput: "#deleteUsersInput",
+		baseCsvInput: "#baseCsvInput",
+		comparedCsvInput: "#comparedCsvInput",	
+    }
+    
+})();
